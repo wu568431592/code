6361240e2f4c3c544e1734c822236de0311af745
wuwenchao <wuwenchao@limikeji.com>
2020-11-28 16:59:20 +0800

    feature flatten

---
+++ src/flatten/index.js
@@ -0,0 +1,25 @@
+function flattenWithReduce(arr){
+  return arr.reduce((pre, next)=>{
+    return pre.concat(Array.isArray(next) ? flattenWithReduce(next): next )
+  },[])
+}
+
+function flattenWithConcat(arr){
+  // while (arr.some(Array.isArray)) {
+  //   arr = [].concat(...arr);
+  // }
+  // return arr
+  const isDeep = arr.some(Array.isArray)
+  if(!isDeep){
+    return arr
+  }
+  // const res = Array.prototype.concat.apply([],arr)
+  const res = [].concat(...arr)
+  return flattenWithConcat(res)
+}
+
+
+module.exports = {
+  flattenWithReduce,
+  flattenWithConcat
+}
