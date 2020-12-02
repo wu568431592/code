332ec4bab6f263f871a8a582fce9e46d241c09af
wuwenchao <wuwenchao@limikeji.com>
2020-12-02 21:41:59 +0800

    feat: vueObserver and wacther

--- src/vueObserve/observe.js
+++ src/vueObserve/observe.js
@@ -5,7 +5,7 @@ function updateView(){
 // 重新定义数组原型
 const oldArrayProperty = Array.prototype;
 const arrProto = Object.create(oldArrayProperty);
-['push', 'pop', 'shift', 'unshift', 'splice'].forEach(method => {
+['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse'].forEach(method => {
   arrProto[method] = function(){
     updateView()
     oldArrayProperty[method].call(this, ...arguments)
@@ -17,15 +17,20 @@ const arrProto = Object.create(oldArrayProperty);
 function defineReactive(target, key, value){
   // 深度监听
   observer(value)
+  const dep = new Dep()
   Object.defineProperty(target,key, {
     get(){
+      if(Dep.target){
+        dep.addSub(Dep.target)
+      }
       return value
     },
     set(newVal){
       // 设置新值时也需要监听
       observer(newVal)
       value = newVal
-      updateView()
+      // updateView()
+      dep.notify()
     }
   })
 }
@@ -42,7 +47,51 @@ function observer(target){
   }
 }
 
-const data = {
+
+
+class Dep{
+  constructor(){
+    this.subs = []
+  }
+  addSub(sub){
+    this.subs.push(sub)
+  }
+  notify(){
+    this.subs.forEach(item =>{
+      item.update()
+    })
+  }
+}
+
+Dep.target = null 
+
+class Watcher{
+  constructor(vm, key, cb){
+    this.vm = vm
+    this.key = key
+    this.cb = cb
+    this.value = this.get()
+  }
+  update(){
+    var value = this.vm.data[this.key];
+    var oldVal = this.value;
+    if (value !== oldVal) {
+      this.value = value;
+      this.cb.call(this.vm, value, oldVal);
+    }
+  }
+  get(){
+    Dep.target = this;  // 缓存自己
+    var value = this.vm.data[this.key]  // 强制执行监听器里的get函数
+    Dep.target = null;  // 释放自己
+    return value;
+  }
+}
+
+
+
+
+var data = {
   name: 'John',
   age: 20,
   info:{
@@ -52,8 +101,13 @@ const data = {
 }
 
 observer(data)
-// data.name = '哈哈哈'
+//模板编译过程中发现有 data引用。就new一个watcher
+new Watcher(this, 'name', function(newVal, oldVal){
+  console.log('name更新了',newVal, oldVal)
+})
+
+data.name = '哈哈哈'
 // data.age = 21
 // data.x = '1000'
 // data.info.address = '上海'
-data.num.push(6)
+// data.num.push(6)
